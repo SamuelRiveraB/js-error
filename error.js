5e8a79f9978efa4542f405a0b4d825d75cbd2b7f
Samuel <samuelrivba@gmail.com>
2024-04-11 20:04:23 -0500

    Add Catch in promises

--- error.js
+++ error.js
@@ -12,3 +12,18 @@ function fail() {
 }
 
 fail();
+
+// Catch in promises
+Promise.resolve("asyncfail")
+  .then((res) => {
+    Promise.resolve()
+      .then(() => {
+        throw new Error("#3 fail");
+      })
+      .catch(console.log);
+    return 5;
+  })
+  .then((res) => console.log(res))
+  .catch((err) => {
+    console.log("Final error", err);
+  });
